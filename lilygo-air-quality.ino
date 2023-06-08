63e38d7d29d7d2081da8717b402d05efc75cc5c8
Greg Heartsfield <scsibug@imap.cc>
2023-06-08 14:05:01 -0500

    working display

--- lilygo-air-quality.ino
+++ lilygo-air-quality.ino
@@ -56,8 +56,8 @@
 #define PIN_IIC_SDA                  18
 
 // Test later without these
-#define PIN_TOUCH_INT                16
-#define PIN_TOUCH_RES                21
+//#define PIN_TOUCH_INT                16
+//#define PIN_TOUCH_RES                21
 
 #define I2C_SDA 43
 #define I2C_SCL 44
@@ -80,6 +80,14 @@ SCD4x scdSensor;
 // ENS160
 SparkFun_ENS160 ensSensor;
 int ensStatus; 
+
+// Variables for Display
+// high quality CO2
+int hq_co2_ppm;
+float temp_c;
+float humidity_rh_perc;
+int voc_ppb;
+int aqi;
 
 void setup()
 {
@@ -114,20 +122,46 @@ void setup()
 	Serial.print("Gas Sensor Status Flag: ");
 	Serial.println(ensStatus);
   Serial.println("Finished ENS sensor init");
+
+
+  // Screen setup. This was in the example but seems unnecessary?
+  //pinMode(PIN_POWER_ON, OUTPUT);
+  //digitalWrite(PIN_POWER_ON, HIGH);
+  Serial.println("About to init TFT");
+  // TFT init causes these errors, but they don't appear fatal
+ //E (3623) gpio: gpio_set_level(226): GPIO output gpio_num error
+ //E (3624) gpio: gpio_set_level(226): GPIO output gpio_num error
+    tft.init();
+    Serial.println("About to set rotation");
+    tft.setRotation(1);
+    tft.fillScreen(TFT_BLACK);
+    tft.fillScreen(TFT_RED); delay(100);
+    tft.fillScreen(TFT_GREEN); delay(100);
+    tft.fillScreen(TFT_BLUE); delay(100);
+    tft.fillScreen(TFT_BLACK); delay(100);
+ // The standard ADAFruit font still works as before
+    tft.setTextColor(TFT_WHITE);
+    tft.setCursor (12, 5);
+    tft.print("Original ADAfruit font!");
+    tft.setTextColor(TFT_WHITE, TFT_BLACK); // Do not plot the background colour
   Serial.println("Exiting Setup");
 }
 
 void scdLoop() {
   if (scdSensor.readMeasurement()) // readMeasurement will return true when fresh data is available
   {
+    hq_co2_ppm = scdSensor.getCO2();
+    temp_c = scdSensor.getTemperature();
+    humidity_rh_perc = scdSensor.getHumidity();
+
     Serial.print(F("CO2(ppm):"));
-    Serial.print(scdSensor.getCO2());
+    Serial.print(hq_co2_ppm);
 
     Serial.print(F("\tTemperature(C):"));
-    Serial.print(scdSensor.getTemperature(), 1);
+    Serial.print(temp_c, 1);
 
     Serial.print(F("\tHumidity(%RH):"));
-    Serial.print(scdSensor.getHumidity(), 1);
+    Serial.print(humidity_rh_perc, 1);
 
     Serial.println();
   }
@@ -139,13 +173,15 @@ void ensLoop()
   // Give values to Air Quality Sensor to improve accuracy
 	//myENS.setTempCompensationCelsius(tempC); 
 	//myENS.setRHCompensationFloat(rh); 
+  voc_ppb = ensSensor.getTVOC();
+  aqi = ensSensor.getAQI();
 	if( ensSensor.checkDataStatus() )
 	{
 		Serial.print("Air Quality Index (1-5) : ");
-		Serial.println(ensSensor.getAQI());
+		Serial.println(aqi);
 
 		Serial.print("Total Volatile Organic Compounds: ");
-		Serial.print(ensSensor.getTVOC());
+		Serial.print(voc_ppb);
 		Serial.println("ppb");
 
 		Serial.print("CO2 concentration: ");
@@ -154,10 +190,31 @@ void ensLoop()
 	}
 }
 
+void tftLoop() {
+  tft.fillScreen(TFT_BLACK);
+  // Show real-time high-quality CO2.
+  int val_col = 90;
+  tft.setTextColor(TFT_WHITE, TFT_BLACK); // Do not plot the background colour
+  tft.drawString("CO2:        ", 5, 0, 2);
+  tft.drawString((String)hq_co2_ppm + " ppm", val_col, 0, 2);
+  // Show VOC in ppb
+  tft.drawString("VOC:       ", 5, 20, 2);
+  tft.drawString((String)voc_ppb+" ppb", val_col, 20, 2);
+  // Show Air Quality Index
+  tft.drawString("AQI:        ", 5, 40, 2);
+  tft.drawString((String)aqi, val_col, 40, 2);
+  // Show Temperature in F
+  tft.drawString("Temp:       ", 5, 60, 2);
+  tft.drawString((String)(temp_c*(9.0/5.0)+32.0)+" F", val_col, 60, 2);
+  // Show Humidity
+  tft.drawString("Humidity:    ", 5, 80, 2);
+  tft.drawString((String)humidity_rh_perc+" %", val_col, 80, 2);
+}
 
 void loop()
 {
  scdLoop();
  ensLoop();
- delay(500);
+ tftLoop();
+ delay(5000);
 }
